f6f27722277e52cf2fc794644a24d52730cf372c
Jesper <jesper@jesper.fi>
2025-08-06 19:16:51 +0300

    Add mobile styling and input field for list component

--- apps/grocery-list/src/app/list/list.ts
+++ apps/grocery-list/src/app/list/list.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MATERIAL_MODULES } from '@angular-monorepo-test/ui';
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule],
+  imports: [CommonModule, ...MATERIAL_MODULES],
   templateUrl: './list.html',
   styleUrl: './list.css',
 })
-export class List {}
+export class List {
+  onClick() {
+    alert('Addd button clicked');
+  }
+}
---
+++ packages/ui/src/lib/material-imports.ts
@@ -0,0 +1,11 @@
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+export const MATERIAL_MODULES = [
+  MatFormFieldModule,
+  MatInputModule,
+  FormsModule,
+  MatIconModule
+];
